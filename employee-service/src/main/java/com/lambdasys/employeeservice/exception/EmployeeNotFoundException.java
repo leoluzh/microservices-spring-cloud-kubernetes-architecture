0378c36cb2479a931402a3d002c3dbd7af43e16e
Leonardo Fernandes <leonardo.l.fernandes@gmail.com>
2021-07-30 00:12:15 -0300

    Classe para entidade nao encontrada.

---
+++ employee-service/src/main/java/com/lambdasys/employeeservice/exception/EmployeeNotFoundException.java
@@ -0,0 +1,13 @@
+package com.lambdasys.employeeservice.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class EmployeeNotFoundException extends Exception {
+
+    public EmployeeNotFoundException(String id){
+        super(String.format("Employee with id %s not found",id));
+    }
+
+}
